cfcdb760f673ae465e2096bbcab864ed6eaef844
dsmith <derek@dsmitts.com>
2011-08-23 21:43:44 -0700

    MOOOORE

--- run.rb
+++ run.rb
@@ -2,6 +2,6 @@ require 'rubygems'
 require 'daemons'
 
 pwd = Dir.pwd
-Daemons.run_proc('git-pull-hook.rb') do
+Daemons.run_proc('git-pull-hook.rb', :dir => '/home/node') do
     exec "ruby /usr/lib/ruby/1.8/git-pull-hook.rb"
 end
