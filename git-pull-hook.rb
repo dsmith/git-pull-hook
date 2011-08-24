93511f888b9ef23254253e6aa2085da1a0d75872
dsmith <derek@dsmitts.com>
2011-08-23 22:27:41 -0700

    simple

--- git-pull-hook.rb
+++ git-pull-hook.rb
@@ -1,29 +1,22 @@
 %w{rubygems sinatra json yaml logger}.each{|x| require x}
 
-log = Logger.new('/var/log/git-pull-hook.log')
+log = Logger.new('/home/node/git-pull-hook.log')
 log.level = Logger::DEBUG
 
-def get_local_path(remote)
-  config =  YAML::load_file("/etc/git-pull-hook/repos.yml")
-  config['repo_home'] + config['repos'][remote]
+def pull(repo)
+  name = repo['repository']['name']
+  `cd ${name} && git pull origin master`
 end
 
-post '/' do
-  push = JSON.parse(params[:payload])
+post '/pull' do
   begin
-    log.debug "Recieved payload: #{params[:payload]}"
-    remote_repo = push['repository']['name']
-    local_repo = get_local_path(remote_repo)
-    `cd #{local_repo} && git pull origin master`
+    pull(JSON.parse(params[:payload]))
     "ok"
   rescue Exception => e
     log.error e
   end
 end
 
-# GET /remote-repo used for debugging
 get '/:remote' do
-  remote_repo = params[:remote]
-  local_repo = get_local_path(remote_repo)
-  `cd #{local_repo} && git pull origin master`
+  "running"
 end
